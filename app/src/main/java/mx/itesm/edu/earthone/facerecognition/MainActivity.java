ffe5e6a3b309994df25888860d6cecacff098076
earthone <daescarcega@gmail.com>
2018-03-22 17:55:52 -0600

    MainActivity Complete

--- app/src/main/java/mx/itesm/edu/earthone/facerecognition/MainActivity.java
+++ app/src/main/java/mx/itesm/edu/earthone/facerecognition/MainActivity.java
@@ -1,34 +1,44 @@
 package mx.itesm.edu.earthone.facerecognition;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
+import com.google.android.gms.vision.face.Landmark;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = "FACE API";
+    private static final String LOG_TAG = "FACE_API";
     private static final int PHOTO_REQUEST = 10;
     private TextView textView;
-    private ImageView imageView;
+    private static ImageView imageView;
     private Uri imageUri;
     private FaceDetector detector;
     private static final int REQUEST_WRITE_PERMISSION = 20;
@@ -53,6 +63,20 @@ public class MainActivity extends AppCompatActivity {
             textView.setText(savedInstanceState.getString(SAVED_INSTANCE_RESULT));
         }
 
+        detector = new FaceDetector.Builder(this)
+        .setTrackingEnabled(false)
+        .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+        .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+        .build();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                        , REQUEST_WRITE_PERMISSION);
+            }
+        });
 
     }
 
@@ -69,21 +93,72 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PHOTO_REQUEST && resultCode == RESULT_OK) {
             launchCamera();
+            imageView.setImageBitmap(null);
 
             try {
-
+                faces();
 
             } catch (Exception e) {
-                Toast.makeText(this, "Failed to load Image", Toast.LENGTH_SHORT).show();
-                Log.e(LOG_TAG, e.toString());
+                e.printStackTrace();
             }
         }
     }
 
+    private void  faces()throws Exception{
+        Bitmap bitmap = decodeBitmapUri(this, imageUri);
+        if(detector.isOperational() && bitmap != null){
+            editedBitmap = Bitmap.createBitmap(bitmap.getWidth(),
+                    bitmap.getHeight(),bitmap.getConfig());
+
+            float scale = getResources().getDisplayMetrics().density;
+            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            paint.setColor(Color.rgb(255, 102,102));
+            paint.setTextSize(14* scale);
+            paint.setShadowLayer(1f, 0f, 1f,Color.GRAY);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(3f);
+
+            Canvas canvas = new Canvas(editedBitmap);
+            canvas.drawBitmap(bitmap, 0,0, paint);
+
+            Frame frame = new Frame.Builder().setBitmap(editedBitmap).build();
+            SparseArray<Face> faces = detector.detect(frame);
+            textView.setText(null);
+            for (int i = 0; i < faces.size(); i++){
+                Face face = faces.get(i);
+                canvas.drawRect(face.getPosition().x,
+                        face.getPosition().y,
+                        face.getPosition().x + face.getWidth(),
+                        face.getPosition().y + face.getHeight(),
+                        paint);
+                textView.setText(textView.getText() + "Face " +  (i+1) +"\n" );
+                textView.setText(textView.getText() + " Smile " +"\n" );
+                textView.setText(textView.getText() +
+                        String.valueOf(face.getIsSmilingProbability() )+ "" );
+
+                for(Landmark landmark : face.getLandmarks()){
+                    int lx = (int) (landmark.getPosition().x);
+                    int ly = (int) (landmark.getPosition().y);
+                    canvas.drawCircle(lx, ly, 10, paint);
+                }
+            }
+
+            if(faces.size() == 0){
+                Toast.makeText(this, "No hay faces", Toast.LENGTH_LONG).show();
+            }else{
+                imageView.setImageBitmap(editedBitmap);
+                textView.setText(textView.getText() + "Faces" +
+                        String.valueOf(faces.size()));
+            }
+        }else {
+            Toast.makeText(this,"Detector  not Working", Toast.LENGTH_LONG).show();
+        }
+    }
 
     private void picTime() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -102,11 +177,13 @@ public class MainActivity extends AppCompatActivity {
             outState.putString(SAVED_INSTANCE_RESULT, textView.getText().toString());
         }
         super.onSaveInstanceState(outState);
+        outState.clear();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        detector.release();
     }
 
     private void launchCamera() {
@@ -131,4 +208,7 @@ public class MainActivity extends AppCompatActivity {
         return BitmapFactory.decodeStream(ctx.getContentResolver()
                 .openInputStream(uri), null, bmOptions);
     }
+
+
+
 }
